4964fe67af9f18256072a78b52e5cbd3310cd9a1
Loezer <loezer.lucas@gmail.com>
2017-01-06 22:10:56 -0200

    Notes: - Implemented a function to get information of a html table with a determined class

--- webscrapy/webscrapy/webscrapy.py
+++ webscrapy/webscrapy/webscrapy.py
@@ -6,6 +6,7 @@ from os import system
 SITE = "http://rank.shoryuken.com/rankings/rank/SF5?alltime=true"
 
 def CheckSite(url):
+    """CheckSite will return the urllib object if the site is available"""
     try:
         html = urlopen(url)
     except HTTPError as e:
@@ -14,6 +15,7 @@ def CheckSite(url):
     return html
 
 def GetTitle(url):
+    """GetTitle will return the website's title if they are available"""
     site = CheckSite(url)
     if site is not None:
         try:
@@ -25,6 +27,7 @@ def GetTitle(url):
     return None
 
 def GetBSObject(url):
+    """GetBSObject will return the Beatiful Soup Object if the site is available, using the parameter "lxml" """
     site = CheckSite(url)
     if site is not None:
         return BeautifulSoup(site.read(), "lxml")
@@ -33,15 +36,31 @@ def GetBSObject(url):
         return None
 
 bsObj = GetBSObject(SITE)
-if bsObj is not None:
-    #print(bsObj)
-    Table = bsObj.find("table", {"class":"table table-striped table-hover table-condensed"}) #Aqui ele procura por uma tabela com essa classe
-    headings = [th.get_text() for th in Table.find("tr").find_all("th")] #o find pega a primeira instancia, entao o primeiro find sao os nomes das colunas!
-    #print(headings)
-    Players = []
-    #Como eu nao quero o primeiro tr (headings), eu pego somente os trs apos ele que seriam as linhas da tabela
-    for row in Table.findAll("tr")[1:]: 
-        #Cada linha tem uma informacao para cada coluna, aqui eu simplemente peguei na posicao 2 do vetor que seria o nome do jogador
-        Players.append(row.find_all("td")[2].get_text()) 
-    for player in Players:
-        print(player)
+#if bsObj is not None:
+#    #print(bsObj)
+#    Table = bsObj.find("table", {"class":"table table-striped table-hover table-condensed"}) #Aqui ele procura por uma tabela com essa classe
+#    headings = [th.get_text() for th in Table.find("tr").find_all("th")] #o find pega a primeira instancia, entao o primeiro find sao os nomes das colunas!
+#    #print(headings)
+#    Players = []
+#    #Como eu nao quero o primeiro tr (headings), eu pego somente os trs apos ele que seriam as linhas da tabela
+#    for row in Table.findAll("tr")[1:]: 
+#        #Cada linha tem uma informacao para cada coluna, aqui eu simplemente peguei na posicao 2 do vetor que seria o nome do jogador
+#        Players.append(row.find_all("td")[2].get_text()) 
+#    for player in Players:
+#        print(player)
+
+
+def GetTableInfo(BSObj, classname):
+    """Return Headings and Rows of a table with de classname parameter"""
+    if BSObj is not None:
+        Table = BSObj.find("table", {"class": classname})
+        Headings = [th.get_text() for th in Table.find("tr").find_all("th")]
+        Rows = []
+        for row in Table.findAll("tr")[1:]:
+            Rows.append([th.get_text() for th in row.find_all("td")])
+        return Headings, Rows
+    else:
+        print("BSObject is None, load the object using GetBSObject before use this function")
+        return None, None
+
+GetTableInfo(bsObj, "table table-striped table-hover table-condensed")
